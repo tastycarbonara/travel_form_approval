e1811b54a32bf3ad37de8a30d5193bc78377d297
tastycarbonara <kevinaurelian1@gmail.com>
2025-10-25 20:10:46 +0700

    check database connection

--- main.go
+++ main.go
@@ -1,15 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello, Go API!")
-    })
+// type Role struct {
+// 	role_id   uint   `gorm:"primaryKey"`
+// 	role_name string `gorm:"unique;not null"`
+// }
 
-    fmt.Println("Server running on port 8080")
-    http.ListenAndServe(":8080", nil)
+// type User struct {
+// 	user_id       uint   `gorm:"primaryKey"`
+// 	user_name     string `gorm:"not null"`
+// 	user_email    string `gorm:"unique;not null"`
+// 	user_password string `gorm:"not null"`
+// }
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var psqlInfo string = CreateConnString()
+
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database:", err)
+	} else {
+		_ = db
+		fmt.Println("connected successfully!")
+	}
+
+	// err = db.AutoMigrate(&Role{}, &User{})
+	// if err != nil {
+	// 	log.Fatal("failed to migrate:", err)
+	// }
+}
+
+func CreateConnString() string {
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	return psqlInfo
 }
