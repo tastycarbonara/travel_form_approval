9c97513aa49aa9de2e7578118def8226de5dc635
tastycarbonara <kevinaurelian1@gmail.com>
2025-10-26 18:55:17 +0700

    feat/made testing endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -25,8 +26,16 @@ func main() {
 	db.DB.AutoMigrate(&models.User{})
 
 	router := mux.NewRouter()
+	router.HandleFunc("/test", TestAPI).Methods("GET")
 	routes.RegisterUserRoutes(router)
 
 	fmt.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func TestAPI(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"status": "API is up and running",
+	})
 }
